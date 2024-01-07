56b67fb5c9638a064d5b2022f8c50433c8b6fb5a
Miconen <micropizza.mc@gmail.com>
2024-01-07 02:04:35 +0200

    Add database initialization

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"tectonic-api/database"
 	"tectonic-api/routes"
 )
 
-// @title		Tectonic API
-// @version		0.1
+// @title			Tectonic API
+// @version			0.1
 // @description		Functionality provider for Tectonic guild.
-// @host		localhost:8080
+// @host			localhost:8080
 // @BasePath		/api
 func main() {
+	conn, err := database.InitDB()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error initializing database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	router := routes.NewAPIBuilder().AttachV1Routes()
 
 	port := os.Getenv("PORT")
@@ -20,9 +30,9 @@ func main() {
 		port = "8080"
 	}
 
-	log.Println("Server will listen on port:", port)
+	fmt.Println("Server will listen on port:", port)
 
-	err := http.ListenAndServe(":"+port, router)
+	err = http.ListenAndServe(":"+port, router)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
