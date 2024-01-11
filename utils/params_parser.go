8b1b77a703d4c12650075b9fa6b6ea269bf3ea21
Roberto Esteves <contact@robertoesteves.dev>
2024-01-11 17:48:56 -0300

    utils(params): support optional parameters and limit usage
    It will now error if the query key is defined more than once.

--- utils/params_parser.go
+++ utils/params_parser.go
@@ -6,19 +6,29 @@ import (
 	"net/http"
 )
 
-// Helper function to check if all parameters were set by the client. It errors
-// if one of more params are empty.
-func ParseParametersURL(r *http.Request, params ...string) (map[string]string, error) {
+// Helper function to check if required parameters were set by the client, and
+// it will error if either the parameter is defined none or more than once.
+//
+// Note that error implements Unwrap() if it's not nill, meaning you can get
+// all errors that ocurred inside the function and send back to the client.
+func ParseParametersURL(r *http.Request, required ...string) (map[string]string, error) {
 	parsed := make(map[string]string)
-	errs := make([]error, 0, len(params))
+	errs := make([]error, 0, len(required))
 
-	for i := range params {
-		value := r.URL.Query().Get(params[i])
-		if value == "" {
-			errs = append(errs, fmt.Errorf("parameter `%s` is empty", params[i]))
+	for k, v := range r.URL.Query() {
+		if len(v) > 1 {
+			errs = append(errs, fmt.Errorf("`%s` was defined more than once", k))
 		}
 
-		parsed[params[i]] = value
+		if v[0] != "" {
+			parsed[k] = v[0]
+		}
+	}
+
+	for i := range required {
+		if _, ok := parsed[required[i]]; !ok {
+			errs = append(errs, fmt.Errorf("`%s` not set", required[i]))
+		}
 	}
 
 	return parsed, errors.Join(errs...)
