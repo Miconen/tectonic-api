1dc83914964b432a4d7470b83a05a859fb3f54c9
Roberto Esteves <contact@robertoesteves.dev>
2024-01-11 07:58:37 -0300

    try to resolve surpefluous writes

--- utils/json_writer.go
+++ utils/json_writer.go
@@ -15,14 +15,19 @@ func JsonWriter(data any) *jsonWriter {
 	}
 }
 
-func (jsonWriter *jsonWriter) ServeHttp(w http.ResponseWriter, r *http.Request) {
-	// We can pass the response writer directly because it won't write the
-	// response if the marshalling had errors.
-	enc := json.NewEncoder(w)
-	err := enc.Encode(jsonWriter.data)
+func (jsonWriter *jsonWriter) IntoHTTP(status int) http.HandlerFunc {
+    // TODO(robertoesteves13): Resolve json writes bug
+    return func (w http.ResponseWriter, _ *http.Request) {
+        w.WriteHeader(status)
 
-	if err != nil {
-		// Can't write JSON, we have a big problem.
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-	}
+        // We can pass the response writer directly because it won't write the
+        // response if the marshalling had errors.
+        enc := json.NewEncoder(w)
+        err := enc.Encode(jsonWriter.data)
+
+        if err != nil {
+            // Can't write JSON, we have a big problem.
+            http.Error(w, "Internal server error", http.StatusInternalServerError)
+        }
+    }
 }
