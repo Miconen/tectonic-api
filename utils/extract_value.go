dd2a6a9b189c12abb7b4035186e2ad2d3be7eac6
Miconen <micropizza.mc@gmail.com>
2024-01-11 14:14:31 +0200

    Add util functions for handlers

---
+++ utils/extract_value.go
@@ -0,0 +1,33 @@
+package utils
+
+import "errors"
+
+// Extract a value from a map, returning a new map
+func ExtractByClone(p map[string]string, key string) (string, map[string]string, error) {
+	val, ok := p[key]
+	if !ok {
+		return "", p, errors.New("No value found for key")
+	}
+
+	// Create a copy of the original map without the guild_id
+	c := make(map[string]string)
+	for k, v := range p {
+		if k != "guild_id" {
+			c[k] = v
+		}
+	}
+
+	return val, c, nil
+}
+
+// Extract a value from a map, mutating the map given
+func ExtractByMutate(p map[string]string, key string) (string, error) {
+	val, ok := p[key]
+	if !ok {
+		return "", errors.New("No value found for key")
+	}
+
+	delete(p, key)
+
+	return val, nil
+}
