db4dc2e0eb6a008a24b8cb935d6f818badbcdd25
Miconen <micropizza.mc@gmail.com>
2024-01-11 14:14:13 +0200

    Update jsonWriter such as to prevent HTTP status rewrites

--- utils/json_writer.go
+++ utils/json_writer.go
@@ -16,18 +16,19 @@ func JsonWriter(data any) *jsonWriter {
 }
 
 func (jsonWriter *jsonWriter) IntoHTTP(status int) http.HandlerFunc {
-    // TODO(robertoesteves13): Resolve json writes bug
-    return func (w http.ResponseWriter, _ *http.Request) {
-        w.WriteHeader(status)
+	// TODO(robertoesteves13): Resolve json writes bug
+	return func(w http.ResponseWriter, _ *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
 
-        // We can pass the response writer directly because it won't write the
-        // response if the marshalling had errors.
-        enc := json.NewEncoder(w)
-        err := enc.Encode(jsonWriter.data)
+		// We can pass the response writer directly because it won't write the
+		// response if the marshalling had errors.
+		enc := json.NewEncoder(w)
+		err := enc.Encode(jsonWriter.data)
 
-        if err != nil {
-            // Can't write JSON, we have a big problem.
-            http.Error(w, "Internal server error", http.StatusInternalServerError)
-        }
-    }
+		if err != nil {
+			// Can't write JSON, we have a big problem.
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+	}
 }
