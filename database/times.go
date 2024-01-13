1197d272c272ead7a7297688337ee1b9b2567cd5
Miconen <micropizza.mc@gmail.com>
2024-01-13 19:41:53 +0200

    Add function that checks for current PB of guild

--- database/times.go
+++ database/times.go
@@ -2,9 +2,12 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"tectonic-api/models"
 	"time"
+
+	"github.com/Masterminds/squirrel"
 )
 
 func SelectTime() (models.Time, error) {
@@ -12,11 +15,33 @@ func SelectTime() (models.Time, error) {
 	return time, nil
 }
 
+func CheckPb(f map[string]string) (int, error) {
+	query := psql.Select("t.time").
+		From("guild_bosses gb").
+		Join("times t ON gb.pb_id = t.run_id").
+		Where(squirrel.Eq{"gb.guild_id": f["guild_id"], "gb.boss": f["boss"]})
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return -1, err
+	}
+
+	row := db.QueryRow(context.Background(), sql, args...)
+
+	var pb int
+
+	err = row.Scan(&pb)
+	if err != nil {
+		return 0, err
+	}
+
+	return pb, nil
+}
+
 func InsertTime(f map[string]string) (models.Time, error) {
 	teamIds := strings.Split(f["user_ids"], ",")
 
 	timeData := map[string]interface{}{
-		"boss_name": f["boss_name"],
+		"boss_name": f["boss"],
 		"date":      time.Now(),
 		"time":      f["time"],
 	}
@@ -45,6 +70,21 @@ func InsertTime(f map[string]string) (models.Time, error) {
 			UserId:  v,
 		})
 	}
+
+	// Update guild pb (guild_bosses.pb_id)
+	sql, args, err = psql.Update("guild_bosses").
+		Set("pb_id", runId).
+		Where(squirrel.Eq{"guild_id": f["guild_id"], "boss": f["boss"]}).
+		ToSql()
+	commandTag, err := db.Exec(context.Background(), sql, args...)
+	if err != nil {
+		return models.Time{}, err
+	}
+
+	if commandTag.RowsAffected() != 1 {
+		return models.Time{}, fmt.Errorf("expected 1 row to be affected, got %d", commandTag.RowsAffected())
+	}
+
 	query := psql.Insert("teams").Columns("run_id", "guild_id", "user_id")
 
 	for _, teammate := range res.Team {
