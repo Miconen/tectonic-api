581a9b765efb43bde60313c264dad4609feb1cc8
Roberto Esteves <contact@robertoesteves.dev>
2025-03-07 14:48:57 -0300

    query detailed users

--- database/utils.go
+++ database/utils.go
@@ -1,18 +1,55 @@
 package database
 
+import (
+	"encoding/json"
+
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type DetailedRsn struct {
+	Rsn   string `json:"rsn"`
+	WomID string `json:"wom_id"`
+}
+
 type UserData struct {
-	UserID  string `json:"user_id"`
-	GuildID string `json:"guild_id"`
-	Points  int32  `json:"points"`
-	RSNs    []Rsn  `json:"rsns"`
+	UserID  string        `json:"user_id"`
+	GuildID string        `json:"guild_id"`
+	Points  int32         `json:"points"`
+	RSNs    []DetailedRsn `json:"rsns"`
 }
 
 type DetailedTime struct {
-	Time      Time       `json:"time"`
-	Teammates []UserData `json:"team"`
+	Time      int32            `json:"time"`
+	BossName  string           `json:"boss_name"`
+	RunID     int32            `json:"run_id"`
+	Date      pgtype.Timestamp `json:"date"`
+	Teammates []UserData       `json:"team"`
 }
 
+// TODO: Probably separate this in multiple queries, my head hurts while
+// making sql queries...
 type DetailedUser struct {
-	User  UserData       `json:"user"`
-	Times []DetailedTime `json:"times"`
+	UserID  string          `json:"user_id"`
+	GuildID string          `json:"guild_id"`
+	Points  int32           `json:"points"`
+	RSNs    json.RawMessage `json:"rsns"`
+	Times   json.RawMessage `json:"times"`
+}
+
+func NewDetailedUserFromRows(rows []GetDetailedUsersRow) []DetailedUser {
+	list := make([]DetailedUser, 0, len(rows))
+
+	for _, row := range rows {
+		user := DetailedUser{
+			UserID:  row.UserID,
+			GuildID: row.GuildID,
+			Points:  row.Points,
+			RSNs:    row.Rsns,
+			Times:   row.Times,
+		}
+
+		list = append(list, user)
+	}
+
+	return list
 }
