b8a340f66f5f078831fba1322bc8a69d32c8c0ea
Miconen <micropizza.mc@gmail.com>
2025-08-16 23:07:22 +0300

    Move usage of environment variables to its own config struct and refactor wom functionality to its own client

--- main.go
+++ main.go
@@ -12,12 +12,18 @@ import (
 
 var log = utils.NewLogger()
 
-//	@title			Tectonic API
-//	@version		0.1
-//	@description	Functionality provider for Tectonic guild.
-//	@host			localhost:8080
-//	@BasePath		/api
+// @title			Tectonic API
+// @version		0.1
+// @description	Functionality provider for Tectonic guild.
+// @host			localhost:8080
+// @BasePath		/api
 func main() {
+	cfg, err := utils.LoadConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading application configuration: %v\n", err)
+		os.Exit(1)
+	}
+
 	conn, err := database.InitDB()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error initializing database: %v\n", err)
@@ -32,18 +38,14 @@ func main() {
 	}
 	log.Info("migrations ran")
 
-	handlers.InitHandlers(conn)
+	wom := utils.NewWomClient(cfg)
+	handlers.InitHandlers(conn, wom)
 	router := routes.NewAPIBuilder().AttachV1Routes()
 	log.Info("routes registered")
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	log.Info("server listening to requests", "port", cfg.Port)
 
-	log.Info("server listening to requests", "port", port)
-
-	err = http.ListenAndServe(":"+port, router)
+	err = http.ListenAndServe(":"+cfg.Port, router)
 	if err != nil {
 		log.Error("Server failed to start", "error", err)
 	}
