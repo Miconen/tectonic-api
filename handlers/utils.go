a52f165b1ee8383c037e7f22fb7822385cfdf53b
Miconen <micropizza.mc@gmail.com>
2025-08-18 04:37:00 +0300

    Fixed foreign constraints not getting caught by error handler

--- handlers/utils.go
+++ handlers/utils.go
@@ -17,73 +17,73 @@ type dbHandler struct {
 
 func (s *Server) getConstraintError(ei database.ErrorInfo) models.APIV1Error {
 	switch ei.Code {
+	case "23503": // Foreign constraint violation
+
+		c := s.constraintsMap[ei.Err.ConstraintName]
+
+		switch c.ForeignTable {
+		case "achievement":
+			return models.ERROR_ACHIEVEMENT_NOT_FOUND
+		case "bosses":
+			return models.ERROR_BOSS_NOT_FOUND
+		case "categories":
+			return models.ERROR_CATEGORY_NOT_FOUND
+		case "event":
+			return models.ERROR_EVENT_NOT_FOUND
+		case "event_participant":
+			return models.ERROR_PARTICIPATION_NOT_FOUND
+		case "guild_bosses":
+			return models.ERROR_GUILD_BOSS_NOT_FOUND
+		case "guild_categories":
+			return models.ERROR_GUILD_CATEGORY_NOT_FOUND
+		case "guilds":
+			return models.ERROR_GUILD_NOT_FOUND
+		case "point_sources":
+			return models.ERROR_POINT_SOURCE_NOT_FOUND
+		case "rsn":
+			return models.ERROR_RSN_NOT_FOUND
+		case "teams":
+			return models.ERROR_TEAM_NOT_FOUND
+		case "times":
+			return models.ERROR_TIME_NOT_FOUND
+		case "user_achievement":
+			return models.ERROR_USER_ACHIEVEMENT_NOT_FOUND
+		case "users":
+			return models.ERROR_USER_NOT_FOUND
+		}
 	case "23505": // Unique constraint violation
 
 		c := s.constraintsMap[ei.Err.ConstraintName]
 
-		switch c.Type {
-		case database.PrimaryKey:
-			switch c.Table {
-			case "achievement":
-				return models.ERROR_ACHIEVEMENT_EXISTS
-			case "bosses":
-				return models.ERROR_BOSS_EXISTS
-			case "categories":
-				return models.ERROR_CATEGORY_EXISTS
-			case "event":
-				return models.ERROR_EVENT_EXISTS
-			case "event_participant":
-				return models.ERROR_PARTICIPATION_EXISTS
-			case "guild_bosses":
-				return models.ERROR_GUILD_BOSS_EXISTS
-			case "guild_categories":
-				return models.ERROR_GUILD_CATEGORY_EXISTS
-			case "guilds":
-				return models.ERROR_GUILD_EXISTS
-			case "point_sources":
-				return models.ERROR_POINT_SOURCE_EXISTS
-			case "rsn":
-				return models.ERROR_RSN_EXISTS
-			case "teams":
-				return models.ERROR_TEAM_EXISTS
-			case "times":
-				return models.ERROR_TIME_EXISTS
-			case "user_achievement":
-				return models.ERROR_USER_ACHIEVEMENT_EXISTS
-			case "users":
-				return models.ERROR_USER_EXISTS
-			}
-		case database.ForeignKey:
-			switch c.ForeignTable {
-			case "achievement":
-				return models.ERROR_ACHIEVEMENT_NOT_FOUND
-			case "bosses":
-				return models.ERROR_BOSS_NOT_FOUND
-			case "categories":
-				return models.ERROR_CATEGORY_NOT_FOUND
-			case "event":
-				return models.ERROR_EVENT_NOT_FOUND
-			case "event_participant":
-				return models.ERROR_PARTICIPATION_NOT_FOUND
-			case "guild_bosses":
-				return models.ERROR_GUILD_BOSS_NOT_FOUND
-			case "guild_categories":
-				return models.ERROR_GUILD_CATEGORY_NOT_FOUND
-			case "guilds":
-				return models.ERROR_GUILD_NOT_FOUND
-			case "point_sources":
-				return models.ERROR_POINT_SOURCE_NOT_FOUND
-			case "rsn":
-				return models.ERROR_RSN_NOT_FOUND
-			case "teams":
-				return models.ERROR_TEAM_NOT_FOUND
-			case "times":
-				return models.ERROR_TIME_NOT_FOUND
-			case "user_achievement":
-				return models.ERROR_USER_ACHIEVEMENT_NOT_FOUND
-			case "users":
-				return models.ERROR_USER_NOT_FOUND
-			}
+		switch c.Table {
+		case "achievement":
+			return models.ERROR_ACHIEVEMENT_EXISTS
+		case "bosses":
+			return models.ERROR_BOSS_EXISTS
+		case "categories":
+			return models.ERROR_CATEGORY_EXISTS
+		case "event":
+			return models.ERROR_EVENT_EXISTS
+		case "event_participant":
+			return models.ERROR_PARTICIPATION_EXISTS
+		case "guild_bosses":
+			return models.ERROR_GUILD_BOSS_EXISTS
+		case "guild_categories":
+			return models.ERROR_GUILD_CATEGORY_EXISTS
+		case "guilds":
+			return models.ERROR_GUILD_EXISTS
+		case "point_sources":
+			return models.ERROR_POINT_SOURCE_EXISTS
+		case "rsn":
+			return models.ERROR_RSN_EXISTS
+		case "teams":
+			return models.ERROR_TEAM_EXISTS
+		case "times":
+			return models.ERROR_TIME_EXISTS
+		case "user_achievement":
+			return models.ERROR_USER_ACHIEVEMENT_EXISTS
+		case "users":
+			return models.ERROR_USER_EXISTS
 		}
 	}
 
