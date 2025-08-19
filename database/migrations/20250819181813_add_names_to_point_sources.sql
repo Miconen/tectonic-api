8dfba73fcd580c0c3ea81a23be8a917037e9a6ca
Miconen <micropizza.mc@gmail.com>
2025-08-19 22:37:34 +0300

    Fix typo in migration

--- database/migrations/20250819181813_add_names_to_point_sources.sql
+++ database/migrations/20250819181813_add_names_to_point_sources.sql
@@ -13,19 +13,19 @@ ALTER COLUMN "name" SET NOT NULL;
 CREATE OR REPLACE FUNCTION insert_default_point_sources()
 RETURNS TRIGGER AS $$
 BEGIN
-  INSERT INTO point_sources (guild_id, source, points)
+  INSERT INTO point_sources (guild_id, source, points, name)
   VALUES
-    (NEW.guild_id, 'event_participation', 5, "Event participation"),
-    (NEW.guild_id, 'event_hosting', 10, "Event hosting"),
-    (NEW.guild_id, 'clan_pb', 10, "Clab PB"),
-    (NEW.guild_id, 'split_low', 10, "Split (Low)"),
-    (NEW.guild_id, 'split_medium', 20, "Split (Medium)"),
-    (NEW.guild_id, 'split_high', 30, "Split (High)"),
-    (NEW.guild_id, 'combat_achievement_low', 5, "Combat Achievement (Low)"),
-    (NEW.guild_id, 'combat_achievement_medium', 10, "Combat Achievement (Medium)"),
-    (NEW.guild_id, 'combat_achievement_high', 15, "Combat Achievement (High)"),
-    (NEW.guild_id, 'combat_achievement_very_high', 25, "Combat Achievement (Very High)"),
-    (NEW.guild_id, 'combat_achievement_highest', 50, "Combat Achievement (Highest)")
+    (NEW.guild_id, 'event_participation', 5, 'Event participation'),
+    (NEW.guild_id, 'event_hosting', 10, 'Event hosting'),
+    (NEW.guild_id, 'clan_pb', 10, 'Clan PB'),
+    (NEW.guild_id, 'split_low', 10, 'Split (Low)'),
+    (NEW.guild_id, 'split_medium', 20, 'Split (Medium)'),
+    (NEW.guild_id, 'split_high', 30, 'Split (High)'),
+    (NEW.guild_id, 'combat_achievement_low', 5, 'Combat Achievement (Low)'),
+    (NEW.guild_id, 'combat_achievement_medium', 10, 'Combat Achievement (Medium)'),
+    (NEW.guild_id, 'combat_achievement_high', 15, 'Combat Achievement (High)'),
+    (NEW.guild_id, 'combat_achievement_very_high', 25, 'Combat Achievement (Very High)'),
+    (NEW.guild_id, 'combat_achievement_highest', 50, 'Combat Achievement (Highest)')
   ON CONFLICT ON CONSTRAINT point_sources_pkey DO NOTHING;
 
   RETURN NEW;
@@ -36,6 +36,9 @@ $$ LANGUAGE plpgsql;
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE "point_sources"
+DROP COLUMN "name";
+
 CREATE OR REPLACE FUNCTION insert_default_point_sources()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -57,7 +60,4 @@ BEGIN
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
-
-ALTER TABLE "point_sources"
-DROP COLUMN "name";
 -- +goose StatementEnd
