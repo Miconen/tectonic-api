9d966a0606bef8d5ebfe5412c3850012ea87a502
Miconen <mico.rintala@gmail.com>
2026-08-26 13:51:19 +0300

    Fix authentication serving early

--- middleware/authentication.go
+++ middleware/authentication.go
@@ -14,7 +14,6 @@ func Authentication(cfg *config.Config) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		logging.Get().Debug("Adding authentication handler")
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r)
 			// Skip auth for docs and OpenAPI spec
 			if !strings.HasPrefix(r.URL.Path, "/api/") {
 				next.ServeHTTP(w, r)
